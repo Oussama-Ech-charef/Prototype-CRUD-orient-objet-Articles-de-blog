1427ac8255d0596f3f8d7ee2fdbd0b18f705c71a
OUSSAMA <oussamacharef71@gmail.com>
2026-03-28 16:12:26 +0100

    create repositori prototype php_s_3 and push file connexion.php

---
+++ connexion.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+class Database {
+    private $host = "localhost";
+    private $db_name = "blog";
+    private $username = "root";
+    private $password = "";
+
+    public $conn;
+
+
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8",
+                $this->username,
+                $this->password
+            );
+
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            
+        } catch (PDOException $o) {
+            echo "Connection error: " . $o->getMessage();
+        }
+        return $this->conn;
+    }
+}
+
+
+
+?>
